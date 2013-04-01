a23e907e27cac22fdc4e8c8ad9aeb4c5fea4c8a3
Sumin Byeon <suminb@gmail.com>
2013-03-31 18:45:18 -0700

    Implemented base62 decoder.

--- app/base62.py
+++ app/base62.py
@@ -1,4 +1,8 @@
 
+#
+# Original PHP code from http://blog.suminb.com/archives/558
+#
+
 CHARSET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 BASE = 62
 
@@ -13,3 +17,21 @@ def encode(n):
     s.reverse()
 
     return ''.join(s)
+
+def decode(b):
+    l, i, v = len(b), 0, 0
+    for x in b:
+        v += __value__(x) * (BASE**(l-(i+1)))
+        i += 1
+
+    return v
+
+def __value__(ch):
+    if ch in '01234567890':
+        return ord(ch) - ord('0')
+    elif ch in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+        return ord(ch) - ord('A') + 10
+    elif ch in 'abcdefghijklmnopqrstuvwxyz':
+        return ord(ch) - ord('a') + 36
+    else:
+        raise RuntimeError('base62: Invalid character (%s)' % ch)

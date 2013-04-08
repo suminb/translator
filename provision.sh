d414bdeb7550cb8c2748ea60152bcec90424aa86
Sumin Byeon <suminb@gmail.com>
2013-04-08 04:36:17 -0700

    Minor adjustments.

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 if [ ! -a /home/vagrant/flag ]; then
     sudo apt-get update
     sudo apt-get -y install python2.7 python-pip
-    sudo apt-get -y install libpq5
+    sudo apt-get -y install libpq5 libpq-dev python-dev
 
     sudo pip install flask-babel flask-sqlalchemy 
     sudo pip install requests

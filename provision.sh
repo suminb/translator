f0e18518b42e4eb9c18aee691fc66295f188f1d8
Sumin Byeon <suminb@gmail.com>
2013-04-10 12:15:12 -0700

    Revised provisioning script.

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -a /home/vagrant/flag ]; then
-    sudo apt-get update
+    #sudo apt-get update
     sudo apt-get -y install python2.7 python-pip
     sudo apt-get -y install libpq5 libpq-dev python-dev
 
@@ -9,6 +9,7 @@ if [ ! -a /home/vagrant/flag ]; then
     sudo pip install requests
     sudo pip install sphinx sphinxcontrib-httpdomain
     sudo pip install psycopg2
+    sudo pip install nilsimsa
 
     touch /home/vagrant/flag
 

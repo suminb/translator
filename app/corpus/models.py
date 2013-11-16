4d53f6c54b1f45ac9cf405a77ea90e120a64200f
Sumin Byeon <suminb@gmail.com>
2013-11-16 11:50:27 -0700

    Record raw corpora.

--- app/corpus/models.py
+++ app/corpus/models.py
@@ -104,3 +104,12 @@ class CorpusIndex(db.Model, BaseModel):
         data['corpus'] = self.corpus.serialize()
 
         return data
+
+
+class CorpusRaw(db.Model, BaseModel):
+    __table_args__ = ( db.UniqueConstraint('hash'), {} )
+
+    id = db.Column(UUID, primary_key=True)
+    hash = db.Column(db.String(255))
+    raw = db.Column(db.Text)
+

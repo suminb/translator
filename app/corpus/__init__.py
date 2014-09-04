22a3d8964fa75bb8228942677c2eec3d5d36ed00
Sumin Byeon <suminb@gmail.com>
2014-09-05 00:52:06 +0900

    Removed corpus dictionary feature

--- app/corpus/__init__.py
+++ app/corpus/__init__.py
@@ -13,35 +13,6 @@ import hashlib
 corpus_module = Blueprint('corpus', __name__, template_folder='templates')
 
 
-@corpus_module.route('/')
-def corpus_list():
-
-    page = int(request.args.get('page', 1))
-    source_lang = request.args.get('sl', None)
-    target_lang = request.args.get('tl', None)
-
-    corpora = Corpus.query \
-        .order_by(Corpus.avg_confidence.desc(), Corpus.frequency.desc())
-
-    # TODO: Move this code to the models class
-    if source_lang != None:
-        corpora = corpora.filter_by(source_lang=source_lang)
-    if target_lang != None:
-        corpora = corpora.filter_by(target_lang=target_lang)
-
-    pagination = Pagination(page=page, total=corpora.count(),
-        per_page=20, 
-        record_name='corpus')
-
-    context = dict(
-        # FIXME: Better way to deal with this?
-        corpora=corpora.offset(pagination.per_page*(page-1)).limit(pagination.per_page),
-        pagination=pagination,
-    )
-
-    return render_template('list.html', **context)
-
-
 @corpus_module.route('/match')
 def corpus_match():
 

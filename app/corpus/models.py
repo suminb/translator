3c059ddb76464c932f5c25a13a200d0df0cd9d07
Sumin Byeon <suminb@gmail.com>
2016-01-29 00:23:39 +0900

    Define DynamoDB model classes

--- app/corpus/models.py
+++ app/corpus/models.py
@@ -1,8 +1,41 @@
-from winnowing import winnow, kgrams, winnowing_hash, sanitize
-
-
-FINGERPRINT_K = 4
+from pynamodb.models import Model
+from pynamodb.attributes import NumberAttribute, UnicodeAttribute, UTCDateTimeAttribute
 
 
 class Corpus():
     pass
+
+
+class Phrase(Model):
+    class Meta:
+        table_name = 'phrase'
+
+    id = NumberAttribute(hash_key=True)
+    observed_at = UTCDateTimeAttribute
+    source_lang = UnicodeAttribute
+    target_lang = UnicodeAttribute
+    source_text = UnicodeAttribute
+    target_text = UnicodeAttribute
+
+
+class Sentence(Model):
+    class Meta:
+        table_name = 'sentence'
+
+    id = NumberAttribute(hash_key=True)
+    observed_at = None
+    source_lang = UnicodeAttribute
+    target_lang = UnicodeAttribute
+    source_text = UnicodeAttribute
+    target_text = UnicodeAttribute
+
+
+if __name__ == '__main__':
+    if not Phrase.exists():
+        Phrase.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
+
+    import pdb
+    pdb.set_trace()
+
+    item = Phrase(1)
+    item.save()

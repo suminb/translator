9d3a74cec21e547977368f6297c88510f349e698
Sumin Byeon <suminb@gmail.com>
2016-02-16 01:18:48 +0900

    In some cases Google Translate returns a string

--- app/api.py
+++ app/api.py
@@ -389,15 +389,20 @@ def __translate__(text, source, target, client='x',
     if client == 'x':
         data = json.loads(req.text)
 
-        try:
-            sentences = data['sentences']
-        except:
-            sentences = data['results'][0]['sentences']
+        # It appears in some cases the Google Translate returns a string
+        # rather than a dictionary
+        if isinstance(data, str):
+            return data
+        else:
+            try:
+                sentences = data['sentences']
+            except TypeError:
+                sentences = data['results'][0]['sentences']
 
-        result = ' '.join(map(lambda x: x['trans'], sentences))
+            result = ' '.join(map(lambda x: x['trans'], sentences))
 
-        # Remove unneccessary white spaces
-        return '\n'.join(map(lambda x: x.strip(), result.split('\n')))
+            # Remove unneccessary white spaces
+            return '\n'.join(map(lambda x: x.strip(), result.split('\n')))
 
     elif client == 't':
         return parse_javascript(req.text)

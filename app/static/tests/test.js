87d2c8ec7c85142244efe5209296ecbbfeefe726
Sumin Byeon <suminb@gmail.com>
2016-03-15 14:12:15 +0000

    Write a test case for extractSentences()

--- app/static/tests/test.js
+++ app/static/tests/test.js
@@ -1,3 +1,31 @@
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
+QUnit.test('extractSentences', function(assert) {
+  var raw = {
+    "sentences":[
+        {
+            "trans":"これはテストです",
+            "orig":"This is a test",
+            "backend":1
+        },
+        {
+            "translit":"Kore wa tesutodesu"
+        }
+    ],
+    "src":"en",
+    "confidence":0.19605306,
+    "ld_result":{
+        "srclangs":[
+            "en"
+        ],
+        "srclangs_confidences":[
+            0.19605306
+        ],
+        "extended_srclangs":[
+            "en"
+        ]
+    }
+  };
+  // Single sentence test
+  assert.equal(extractSentences(raw), 'これはテストです');
+
+  // TODO: Multi-sentence test
 });

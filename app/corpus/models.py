f914772ccaf85028d30e4fbdbf8f531035a4147f
Sumin Byeon <suminb@gmail.com>
2016-11-13 01:46:04 +0900

    Force AWS region

--- app/corpus/models.py
+++ app/corpus/models.py
@@ -6,6 +6,9 @@ class Translation(Model):
     class Meta:
         table_name = 'translation'
 
+        # NOTE: Could we make this to follow the default settings?
+        region = 'us-west-2'
+
     timestamp = UTCDateTimeAttribute(range_key=True)
     raw = UnicodeAttribute()
     hash = UnicodeAttribute(hash_key=True)

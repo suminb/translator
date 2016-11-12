9aa9844a8d207e20bab1b28f11a0c03185ccf2be
Sumin Byeon <suminb@gmail.com>
2016-11-13 01:36:27 +0900

    Store raw data in DynamoDB

--- app/corpus/models.py
+++ app/corpus/models.py
@@ -1,2 +1,23 @@
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute
+
+
 class Corpus():
     pass
+
+
+class Translation(Model):
+    class Meta:
+        table_name = 'translation'
+
+    timestamp = UTCDateTimeAttribute(range_key=True)
+    raw = UnicodeAttribute()
+    hash = UnicodeAttribute(hash_key=True)
+    source_lang = UnicodeAttribute()
+    target_lang = UnicodeAttribute()
+
+
+if __name__ == '__main__':
+    if not Translation.exists():
+        Translation.create_table(read_capacity_units=1,
+                                 write_capacity_units=1, wait=True)

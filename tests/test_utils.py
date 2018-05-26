6b53c6d5d27ae20f9c63d91393d6a08154ecce99
Sumin Byeon <suminb@gmail.com>
2018-05-27 02:54:43 +0900

    Write some test cases for utils module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
-from translator.utils import HTTPException
+from translator.utils import (
+    HTTPException, language_options, language_options_html,
+    parse_javascript)
 
 
 def test_http_exception():
@@ -8,3 +10,21 @@ def test_http_exception():
     except HTTPException as e:
         assert 'An HTTP exception' == e.message
         assert 500 == e.status_code
+
+
+def test_language_options():
+    options = language_options()
+    for lang, name in options:
+        assert isinstance(lang, str)
+        assert isinstance(name, str)
+
+
+def test_language_options_html():
+    options = language_options_html()
+    for option in options.split('\n'):
+        assert option.startswith('<option')
+
+
+def test_parse_javascript():
+    assert parse_javascript('[,1,,2,,,3]') == \
+        [None, 1, None, 2, None, None, 3]
